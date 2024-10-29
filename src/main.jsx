c30e8cfc83f870bad40099a17bccdaf01d5741eb
IsMa <itorre13@hotmail.com>
2024-10-29 12:19:43 +0100

    proyecto final frontend 88%

--- src/main.jsx
+++ src/main.jsx
@@ -9,26 +9,24 @@ import './index.css'
 
 const router = createBrowserRouter([
   {
-    path : "/",
+    path: "/",
     element : <Inicio />,
-    errorElement : <Error />,
-    children : [
-      {
-        path : "formulario",
-        element : <App />,
-        errorElement : <Error />
-      },
-      {
-        path : "rutas",
-        element : <Rutas />,
-        errorElement : <Error />
-      }
-    ]
+    errorElement : <Error />
   },
-]);
+  {
+    path: "/formulario",
+    element : <App />,
+    errorElement : <Error />
+  },
+  {
+    path: "/rutas",
+    element : <Rutas />,
+    errorElement : <Error />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={ router } future={{ v7_startTransition: true }}/>
+    <RouterProvider router={ router }/>
   </StrictMode>,
 )
